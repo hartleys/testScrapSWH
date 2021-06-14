e8b2797094be04476d9ff7ae5c262c7d461ab604
Steve Hartley <stephen.hartley.phd@gmail.com>
2021-06-14 14:38:50 -0400

    v002

--- test.scrap.R
+++ test.scrap.R
@@ -4,22 +4,25 @@ for( ss in samplist ){
   message("-",ss);
   sampCounts[[ss]] <- list();
   kk <- dda$sample == ss & dda$IS_SCNA == 1;
+  kkn <-  dda$sample == ss & (dda$IS_SCNA == 1 | dda$event.type == "NA");
   ddax <- dda[kk,]
-        kkn <-  dda$sample == ss & (dda$IS_SCNA == 1 | dda$event.type == "NA" | dda$event.type == "???");
-        ddaxn <- dda[kkn,]
+  ddaxn <- dda[kkn,]
   multidata.info[[ p(ss) ]] <- list()
   multidata.info[[ p(ss) ]][["data"]] <- ddaxn;
   multidata.info[[ p(ss) ]][["events"]] <- character();
+  multidata.info[[ p(ss) ]][["fullchrom"]] <- list();
+  multidata.info[[ p(ss) ]][["fullchrom.FULL"]] <- list();
+  multidata.info[[ p(ss) ]][["flags"]] <- c();
   multidata.info[[ p(ss) ]][["events.clonal"]] <- character();
   multidata.info[[ p(ss) ]][["events.info"]] <- list();
   multidata.info[[ p(ss) ]][["events.idx"]]  <- list();
   multidata.info[[ p(ss) ]][["events.data"]] <- list();
-  multidata.info[[ p(ss) ]][["fullchrom"]] <- list();
-  multidata.info[[ p(ss) ]][["flags"]] <- c();
+
+
 
     sampCounts[[ss]][[p("rawCT.ALL")]] <- sum(kk);
     sampCounts[[ss]][[p("chrCT.ALL")]] <- 0;
-    sampCounts[[ss]][[p("mrgCT.ALL")]] <- 0;
+    #sampCounts[[ss]][[p("mrgCT.ALL")]] <- 0;
     for(cc in chromlist){
       kkx  <- kk  & dda$Chromosome == cc
       kkxc <- kkx & dda$event.clonality == "clonal"
@@ -27,10 +30,56 @@ for( ss in samplist ){
         sampCounts[[ss]][[p("chrCT.ALL")]] <- sampCounts[[ss]][[p("chrCT.ALL")]]+1;
       }
     }
-  #for(ee in eventlist ){
-  #  kk <- dda$sample == ss & dda$event.type == ee & dda$IS_SCNA == 1;
-  #  print(dda[kk,])
-  #}
+
+    for(cc in chromlist){
+      kk <- dda$sample == ss & dda$Chromosome == cc
+      if(cc %in% acrocentric.chroms){
+        centromere.end <- centromere.data$chromEnd[ centromere.data$chr == cc ]
+        kk <- kk & dda$End_bp >= centromere.end
+      }
+      event.listing <- unique(dda$event.type.v5[kk])
+      event.listing <- event.listing[ ! event.listing %in% c("NA") ]
+      simple.ee <- event.listing;
+      if(length(event.listing) == 1 && event.listing %in% c("deletion","gain","CopyNeutralLOH")){
+        if( any(dda$event.clonality[kk] == "clonal")){
+           event.listing <- p("clonal.",event.listing);
+        }
+        multidata.info[[ p(ss) ]][["fullchrom"]][[ length(multidata.info[[ p(ss) ]][["fullchrom"]]) + 1 ]] <-
+            list(chrom=cc,ee=event.listing,event=simple.ee,flag=FALSE)
+      } else {
+        event.listing <- event.listing[ ! event.listing %in% c("???") ]
+        if(length(event.listing) == 1 && event.listing %in% c("deletion","gain","CopyNeutralLOH")){
+          if( any(dda$event.clonality[kk] == "clonal")){
+             event.listing <- p("clonal.",event.listing);
+          }
+          multidata.info[[ p(ss) ]][["fullchrom"]][[ length(multidata.info[[ p(ss) ]][["fullchrom"]]) + 1 ]] <-
+              list(chrom=cc,ee=event.listing,event=simple.ee,flag=TRUE) 
+        }
+      }
+    }
+    for(cc in chromlist){
+      kk <- dda$sample == ss & dda$Chromosome == cc
+      event.listing <- unique(dda$event.type.v5[kk])
+      event.listing <- event.listing[ ! event.listing %in% c("NA") ]
+      simple.ee <- event.listing;
+      if(length(event.listing) == 1 && event.listing %in% c("deletion","gain","CopyNeutralLOH")){
+        if( any(dda$event.clonality[kk] == "clonal")){
+           event.listing <- p("clonal.",event.listing);
+        }
+        multidata.info[[ p(ss) ]][["fullchrom.FULL"]][[ length(multidata.info[[ p(ss) ]][["fullchrom.FULL"]]) + 1 ]] <-
+            list(chrom=cc,ee=event.listing,event=simple.ee,flag=FALSE)
+      } else {
+        event.listing <- event.listing[ ! event.listing %in% c("???") ]
+        if(length(event.listing) == 1 && event.listing %in% c("deletion","gain","CopyNeutralLOH")){
+          if( any(dda$event.clonality[kk] == "clonal")){
+             event.listing <- p("clonal.",event.listing);
+          }
+          multidata.info[[ p(ss) ]][["fullchrom.FULL"]][[ length(multidata.info[[ p(ss) ]][["fullchrom.FULL"]]) + 1 ]] <-
+              list(chrom=cc,ee=event.listing,event=simple.ee,flag=TRUE) 
+        }
+      }
+    }
+
 
   for(ee in eventlist ){
     kk <- dda$sample == ss & dda$event.type == ee & dda$IS_SCNA == 1;
@@ -46,11 +95,11 @@ for( ss in samplist ){
       kkxc <- kkx & dda$event.clonality == "clonal"
       if(sum(kkx) > 0){
         sampCounts[[ss]][[p("chrCT.",ee)]] <- sampCounts[[ss]][[p("chrCT.",ee)]]+1;
-        idx <- which(kkx | (dda$sample == ss & (dda$event.info == "NA" | dda$event.type == "???") & dda$Chromosome == cc))
-        while( length(idx) > 0 && ("NA" == ( dda$event.type[idx[1]] ) | "???" == ( dda$event.type[idx[1]] ) )     ){
+        idx <- which(kkx | (dda$sample == ss & dda$event.info == "NA" & dda$Chromosome == cc))
+        while( length(idx) > 0 && "NA" == ( dda$event.type[[ idx[[1]] ]] )){
           idx <- idx[-1];
         }
-        while( length(idx) > 0 && ("NA" == ( dda$event.type[[ idx[[length(idx)]] ]] ) | "???" == ( dda$event.type[[ idx[[length(idx)]] ]] )  )){
+        while( length(idx) > 0 && "NA" == ( dda$event.type[[ idx[[length(idx)]] ]] )){
           idx <- idx[-length(idx)];
         }
 
@@ -60,16 +109,15 @@ for( ss in samplist ){
           if( sum(kkxc) > 0){
             sampCounts[[ss]][[p("mrgCT.clonal.",ee)]] <- sampCounts[[ss]][[p("mrgCT.clonal.",ee)]]+1;
             multidata.info[[ p(ss) ]][["events.clonal"]] <- c(multidata.info[[ p(ss) ]][["events.clonal"]],p(ee,":chr",cc))
-
           }
           is.clonal <- sum(kkxc) > 0
           clonality.string <- ifelse(sum(kkxc) > 0,"clonal","nonclonal")
 
-            ddax <- multidata.info[[ p(ss) ]][["data"]]
-            zzix <- which( ddax$rownum == dda$rownum[ idx[1] ])
-            ddax$notes[ zzix ] <- pd(ddax$notes[ zzix ],p("SINGLETON[",ee,"]"))
-            multidata.info[[ p(ss) ]][["data"]] <- ddax;
-            multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
+          ddax <- multidata.info[[ p(ss) ]][["data"]]
+          zzix <- which( ddax$rownum == dda$rownum[ idx[1] ])
+          ddax$notes[ zzix ] <- pd(ddax$notes[ zzix ],p("SINGLETON[",ee,"]"))
+          multidata.info[[ p(ss) ]][["data"]] <- ddax;
+          multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
                   multidata.info[[ p(ss) ]][["events.info"]][[ length(multidata.info[[ p(ss) ]][["events.idx"]])+1  ]] <- list(
                       event = ee,chrom=cc,
                       start = ddax$Start_bp[[ zzix ]],
@@ -81,17 +129,20 @@ for( ss in samplist ){
                   #message("class: ",class( multidata.info[[ p(ss) ]][["events.idx"]] ))
                   multidata.info[[ p(ss) ]][["events.idx"]][[ length(multidata.info[[ p(ss) ]][["events.idx"]])+1  ]] <- c(zzix);
                   multidata.info[[ p(ss) ]][["events.data"]][[ length(multidata.info[[ p(ss) ]][["events.data"]])+1  ]] <- ddax[zzix,,drop=F]
+
+
         } else if(length(idx) > 1){
           ddax <- multidata.info[[ p(ss) ]][["data"]]
           message("Multi On Chrom: ",ss,"/",cc);
           multi.on.chrom.ct = multi.on.chrom.ct  + 1;
           sampCounts[[ss]][[p("mrgCT.",ee)]] <- sampCounts[[ss]][[p("mrgCT.",ee)]]+1;
+          multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
+              message("----------");
+              message("mrgCT.",ee," = ", sampCounts[[ss]][[p("mrgCT.",ee)]] )
+              print( multidata.info[[ p(ss) ]][["events"]] )
           zzix <- which( ddax$rownum == dda$rownum[ idx[1] ])
           ddax$notes[ zzix ] <- pd(ddax$notes[ zzix ],p("START[",ee,"]"))
           if(DEBUG.MODE){ message("notes[i=",1,"]/[idx=",idx[1],"]/[ddax=",(zzix),"]=END") }
-          multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
-          #multidata.info[[ p(ss) ]][["events.info"]] <- c(multidata.info[[ p(ss) ]][["events.info"]],p(ee,":chr",cc,":", ddax$Start_bp[[zzix]],":"  ))
-
           for(i in seq_along(idx)[-1]){
             if(DEBUG.MODE){ message("i=",i) }
             if(DEBUG.MODE){ print(dda[idx[(i-1):(i)],c("sample","Chromosome","Start_bp","End_bp","event.type","rownum","notes")] )}
@@ -110,13 +161,15 @@ for( ss in samplist ){
               zzix.last  <- zzix.curr[[length(zzix.curr)]]
               is.clonal  <- any( f.na( ddax$event.clonality[zzix.curr] == "clonal" ))
               is.event   <- any( f.na( ddax$event.type[zzix.curr] == ee ))
-              clonality.string <- ifelse(is.clonal,"clonal","nonclonal")
+          clonality.string <- ifelse(sum(kkxc) > 0,"clonal","nonclonal")
+
               if(is.clonal & is.event){
                 sampCounts[[ss]][[p("mrgCT.clonal.",ee)]] <- sampCounts[[ss]][[p("mrgCT.clonal.",ee)]]+1;
                 multidata.info[[ p(ss) ]][["events.clonal"]] <- c(multidata.info[[ p(ss) ]][["events.clonal"]],p(ee,":chr",cc))
               }
               if(! is.event ){
-                message("DELETING EVENT!")
+                message("----------- NON EVENT: ",ss," / ",cc ," / ",ee);
+                print(ddax[zzix.curr,])
                 sampCounts[[ss]][[p("mrgCT.",ee)]] <- sampCounts[[ss]][[p("mrgCT.",ee)]]-1;
                 multidata.info[[ p(ss) ]][["events"]] <- multidata.info[[ p(ss) ]][["events"]][- length(multidata.info[[ p(ss) ]][["events"]])]
               } else {
@@ -133,14 +186,17 @@ for( ss in samplist ){
                   multidata.info[[ p(ss) ]][["events.idx"]][[ length(multidata.info[[ p(ss) ]][["events.idx"]])+1  ]] <- zzix.curr;
                   multidata.info[[ p(ss) ]][["events.data"]][[ length(multidata.info[[ p(ss) ]][["events.data"]])+1  ]] <- ddax[zzix.curr,,drop=F]
               }
-
-              if(any( dda$event.type[idx[i:length(i)]] == ee )){
+              if(any( dda$event.type[idx[i:length(idx)]] == ee )){
                 if(DEBUG.MODE){ message("BREAK-step 2") }
                 sampCounts[[ss]][[p("mrgCT.",ee)]] <- sampCounts[[ss]][[p("mrgCT.",ee)]]+1;
+                multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
+                  message("----------");
+                  message("mrgCT.",ee," = ", sampCounts[[ss]][[p("mrgCT.",ee)]] )
+                  print( multidata.info[[ p(ss) ]][["events"]] )
+  
                 zzix <- which( ddax$rownum == dda$rownum[ idx[i] ])
                 ddax$notes[ zzix ] <- pd(ddax$notes[ zzix ],p("START[",ee,"]"))
                 if(DEBUG.MODE){ message("notes[i=",i,"]/[idx=",idx[i],"]/[ddax=",(zzix),"]=START") }
-                multidata.info[[ p(ss) ]][["events"]] <- c(multidata.info[[ p(ss) ]][["events"]],p(ee,":chr",cc))
               }
             }  
           }
@@ -155,8 +211,9 @@ for( ss in samplist ){
               zzix.curr  <- which( ddax$rownum %in% dda$rownum[ idx[idx.curr] ])
               zzix.last  <- zzix.curr[[length(zzix.curr)]]
               is.clonal  <- any( f.na( ddax$event.clonality[zzix.curr] == "clonal" ))
-              is.event   <- any( f.na( ddax$event.type[zzix.curr] == ee ))
               clonality.string <- ifelse(is.clonal,"clonal","nonclonal")
+              is.event   <- any( f.na( ddax$event.type[zzix.curr] == ee ))
+
               if(is.clonal){
                 sampCounts[[ss]][[p("mrgCT.clonal.",ee)]] <- sampCounts[[ss]][[p("mrgCT.clonal.",ee)]]+1;
                 multidata.info[[ p(ss) ]][["events.clonal"]] <- c(multidata.info[[ p(ss) ]][["events.clonal"]],p(ee,":chr",cc))
@@ -184,9 +241,9 @@ for( ss in samplist ){
 
 
   }
+
   sampCounts[[ss]][[p("mrgCT.ALL")]] <- 0;
   for(ee in eventlist ){
     sampCounts[[ss]][[p("mrgCT.ALL")]] <- sampCounts[[ss]][[p("mrgCT.ALL")]] + sampCounts[[ss]][[p("mrgCT.",ee)]]
   }
-
 }
